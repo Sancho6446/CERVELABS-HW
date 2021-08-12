5d2c84354202ff3104e207a608f83e6230f72153
Sancho6446 <70781480+Sancho6446@users.noreply.github.com>
2021-08-12 19:54:40 -0300

    Para mvp una canilla con las contras barras

--- CERVELABS-HW-main/Proyecto2021/NodeMCU32/nodeConTiempo/nodeConTiempo.ino
+++ CERVELABS-HW-main/Proyecto2021/NodeMCU32/nodeConTiempo/nodeConTiempo.ino
@@ -8,7 +8,7 @@ const char* password = "0141840184";
 /*const char* ssid = "EDU-SPECIAL";
 const char* password = "TiC_1996$";*/
 String serverName = "https://cervelabs.herokuapp.com/arduino/pub/add-record";
-String datos;
+String datos= "0";
 long lastTime = 0;
 long minutes = 0;
 long hours = 0;
@@ -19,8 +19,13 @@ void setup() {
 Serial.begin(115200);
 }
 void loop() { 
-datos = Serial.readString();
-if(datos.indexOf("*") > 0){
+if (Serial.available() > 0) {
+Caudalimetrol = Serial.readStringUntil('+');
+Caudalimetro2 = Serial.readStringUntil('*');
+}
+if(Caudalimetrol == NULL){Caudalimetrol = 0.0;}
+if(Caudalimetro2 == NULL){Caudalimetro2 = 0.0;}
+/*if(datos.indexOf("*") > 0){
     datos.replace("*", "");
     Caudalimetrol = datos;
  
@@ -28,20 +33,19 @@ if(datos.indexOf("*") > 0){
     if(datos.indexOf("+") > 0){
     datos.replace("+", "");
     Caudalimetro2 = datos;
- 
+  Serial.println(Caudalimetro2);
     }
     if(datos.indexOf("~") > 0){
     datos.replace("~", "");
-    Caudalimetro3 = datos;
+    Caudalimetro3 = datos; 
  
-    }
-   if(millis()-lastTime > 60000){//60000 == 60 segundos
+    }*/
+   if(millis()-lastTime > 10000){//60000 == 60 segundos
     minutes++;
-    Serial.println(minutes);
     lastTime = millis();
     
   }
-  if(minutes >= 60){
+  if(minutes >= 2){
     hours++;
     minutes = 0;
     WiFi.begin(ssid, password);
@@ -55,8 +59,10 @@ if(datos.indexOf("*") > 0){
       http.addHeader("Content-Type", "application/json");
     const int capacity = JSON_OBJECT_SIZE(2)+64;
     StaticJsonDocument<capacity> doc; 
-    doc["pubId"] = "60e8797fde943f2afc94b8b2"; 
-    doc["record"] = String(Caudalimetrol +",10-"+ Caudalimetro2 +",3-"+ Caudalimetro3 +",21");
+    doc["pubId"] = "60e8797fde943f2afc94b8b2";
+     
+    doc["record"] = String(Caudalimetro2+",10-0,3-0,21");
+    //doc["record"] = String(Caudalimetrol +",10-"+ Caudalimetro2 +",3-"+ Caudalimetro3 +",21");
      
      String json;
      serializeJson(doc, json);
