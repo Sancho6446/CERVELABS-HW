51fe81329b1c282c14a6f747d5a5d5e3e996fb22
Sancho6446 <70781480+Sancho6446@users.noreply.github.com>
2021-09-03 10:33:33 -0300

    Add files via upload

---
+++ CERVELABS-HW-main/CERVELABS-HW-main/Proyecto2021/NodeMCU32/Contar_una_hora/Contar_una_hora.ino
@@ -0,0 +1,25 @@
+long lastTime = 0;
+long minutes = 0;
+long hours = 0;
+
+void setup() {
+Serial.begin(115200); 
+}
+
+void loop() {
+  if(millis()-lastTime > 60000){
+    minutes++;
+    lastTime = millis();
+    Serial.print(minutes);
+  }
+  if(minutes > 6){
+    hours++;
+    Serial.print(hours);
+    minutes = 0;
+    
+  }
+  
+
+
+  
+}
